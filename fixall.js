a55a83a31703528e95e8c25a1ed82f3617316fdf
Jesper S. Sandal <jespersandal@outlook.com>
2016-11-25 15:07:51 +0100

    Update fixall.js

--- fixall.js
+++ fixall.js
@@ -41,6 +41,7 @@ function fixAll() {
                 unfixed = unfixed.replace(/\(\s/g, "(");
                 unfixed = unfixed.replace(/\[aid:\s/g, "[aid:");
                 unfixed = unfixed.replace(/\n\n\n/g, "\n\n");  
+                unfixed = unfixed.replace(/\n+$/, "");
                 for (i=0; i<unfixed.length; i++) {
                     if(unfixed[i] == '"'){
                         var sentenceloop = true;
